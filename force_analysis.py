69bb6f7ae354d139eac76340d494a04280474a4c
cdkane9 <colemankane@boisestate.edu>
2026-06-18 15:28:15 -0600

    fixed bug

--- force_analysis.py
+++ force_analysis.py
@@ -490,7 +490,7 @@ if __name__ == '__main__':
         obj_func=selected_obj_func
     )
 
-    if args.obj_func not in ['ram_smp', 'smp_ram']:
+    if args.obj_func == 'smp_scope':
         print('wrong branch')
         print(f'Median cosine distance score: {np.nanmedian(cos_scores):.4f}')
 
